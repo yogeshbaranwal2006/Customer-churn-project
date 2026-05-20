592f6228813bcc556efb6654dbe3bd7db7b590a0
Yogesh Baranwal <yogeshnovember@gmail.com>
2026-05-20 16:17:41 +0530

    Updated API URL

--- frontend/app.py
+++ frontend/app.py
@@ -195,7 +195,7 @@ if st.button("Predict"):
     }
 
     response = requests.post(
-        "http://127.0.0.1:8000/predict",
+        "https://customer-churn-api-7nqw.onrender.com/predict",
         json=data
     )
 
